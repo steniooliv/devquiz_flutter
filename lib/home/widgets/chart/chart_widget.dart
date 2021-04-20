76798da0d307ac42d055564d901dbc049e622f26
Stenio Oliveira <steniooliv@gmail.com>
2021-04-20 00:41:43 -0300

    create progress chart widget

---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,37 @@
+import 'package:devquiz_flutter/core/app_colors.dart';
+import 'package:devquiz_flutter/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  const ChartWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 80,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: 80,
+              height: 80,
+              child: CircularProgressIndicator(
+                strokeWidth: 10,
+                value: 0.75,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor:
+                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "75%",
+              style: AppTextStyles.heading,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
