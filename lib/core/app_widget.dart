dcf442c3e39ba9750da7d3ded32d67928c9da9b4
Stenio Oliveira <steniooliv@gmail.com>
2021-04-21 23:23:20 -0300

    change home to HomePage

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,6 +1,4 @@
-import 'package:devquiz_flutter/challenge/challenge_page.dart';
 import 'package:devquiz_flutter/home/home_page.dart';
-import 'package:devquiz_flutter/splash/spash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -8,7 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: ChallengePage(),
+      home: HomePage(),
     );
   }
 }
