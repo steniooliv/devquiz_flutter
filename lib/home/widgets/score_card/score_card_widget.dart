4e7705fdd6a758a465e81cab528eb309479fbfdd
Stenio Oliveira <steniooliv@gmail.com>
2021-04-21 23:24:47 -0300

    add score from database

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -4,7 +4,9 @@ import 'package:devquiz_flutter/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
-  const ScoreCardWidget({Key? key}) : super(key: key);
+  final int score;
+
+  const ScoreCardWidget({Key? key, required this.score}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +24,9 @@ class ScoreCardWidget extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: ChartWidget(),
+              child: ChartWidget(
+                score: score,
+              ),
             ),
             Expanded(
               flex: 3,
