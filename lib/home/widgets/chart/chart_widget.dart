af523a71be1ebca140304903e588009455746a56
Stenio Oliveira <steniooliv@gmail.com>
2021-04-21 23:25:55 -0300

    integrade score on chart from database

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -3,7 +3,9 @@ import 'package:devquiz_flutter/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class ChartWidget extends StatelessWidget {
-  const ChartWidget({Key? key}) : super(key: key);
+  final int score;
+
+  const ChartWidget({Key? key, required this.score}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,7 +19,7 @@ class ChartWidget extends StatelessWidget {
               height: 80,
               child: CircularProgressIndicator(
                 strokeWidth: 10,
-                value: 0,
+                value: score / 100,
                 backgroundColor: AppColors.chartSecondary,
                 valueColor:
                     AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
@@ -26,7 +28,7 @@ class ChartWidget extends StatelessWidget {
           ),
           Center(
             child: Text(
-              "0%",
+              "$score%",
               style: AppTextStyles.heading,
             ),
           )
