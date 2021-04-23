55711c2a17482d6b2111116ca91e315d3b27eb7e
Stenio Oliveira <steniooliv@gmail.com>
2021-04-23 01:36:47 -0300

    create buttons widgets

---
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,73 @@
+import 'package:devquiz_flutter/core/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NextButtonWidget extends StatelessWidget {
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+  final Color borderColor;
+  final Color overlayColor;
+  final VoidCallback onTap;
+
+  NextButtonWidget({
+    required this.label,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.borderColor,
+    required this.overlayColor,
+    required this.onTap,
+  });
+
+  NextButtonWidget.green({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.darkGreen,
+        this.textColor = AppColors.white,
+        this.borderColor = AppColors.darkGreen,
+        this.overlayColor = AppColors.white.withAlpha(50),
+        this.label = label,
+        this.onTap = onTap;
+
+  NextButtonWidget.white({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.white,
+        this.textColor = AppColors.grey,
+        this.borderColor = AppColors.lightGrey,
+        this.overlayColor = AppColors.black.withAlpha(50),
+        this.label = label,
+        this.onTap = onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            backgroundColor,
+          ),
+          overlayColor: MaterialStateProperty.all(
+            overlayColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          side: MaterialStateProperty.all(
+            BorderSide(
+              color: borderColor,
+            ),
+          ),
+        ),
+        onPressed: onTap,
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 15,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
