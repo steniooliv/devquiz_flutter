79029b094353e07e33f24bbdedc3dd2f30f17cde
Stenio Oliveira <steniooliv@gmail.com>
2021-04-24 23:37:05 -0300

    create animate to chart

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -2,37 +2,67 @@ import 'package:devquiz_flutter/core/app_colors.dart';
 import 'package:devquiz_flutter/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class ChartWidget extends StatelessWidget {
-  final int score;
+class ChartWidget extends StatefulWidget {
+  final double score;
 
   const ChartWidget({Key? key, required this.score}) : super(key: key);
+
+  @override
+  _ChartWidgetState createState() => _ChartWidgetState();
+}
+
+class _ChartWidgetState extends State<ChartWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void _initAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _animation =
+        Tween<double>(begin: 0.0, end: widget.score).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void initState() {
+    _initAnimation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 80,
       height: 80,
-      child: Stack(
-        children: [
-          Center(
-            child: Container(
-              width: 80,
-              height: 80,
-              child: CircularProgressIndicator(
-                strokeWidth: 10,
-                value: score / 100,
-                backgroundColor: AppColors.chartSecondary,
-                valueColor:
-                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, _) => Stack(
+          children: [
+            Center(
+              child: Container(
+                width: 80,
+                height: 80,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  value: _animation.value,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
               ),
             ),
-          ),
-          Center(
-            child: Text(
-              "$score%",
-              style: AppTextStyles.heading,
-            ),
-          )
-        ],
+            Center(
+              child: Text(
+                "${(_animation.value * 100).toInt()}%",
+                style: AppTextStyles.heading,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
