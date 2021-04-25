707e2fffa528759f62fc2c6cf45df33eb739714d
Stenio Oliveira <steniooliv@gmail.com>
2021-04-24 23:37:29 -0300

    return title to home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -67,6 +67,7 @@ class _HomePageState extends State<HomePage> {
                               CupertinoPageRoute(
                                   builder: (context) => ChallengePage(
                                         question: e.questions,
+                                        title: e.title,
                                       )),
                             );
                           },
