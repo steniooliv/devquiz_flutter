4f3d378cbcb74c592839f5535e1e5fff8f16626d
Stenio Oliveira <steniooliv@gmail.com>
2021-04-24 23:36:24 -0300

    create navigate to home page

--- lib/splash/spash_page.dart
+++ lib/splash/spash_page.dart
@@ -1,10 +1,21 @@
 import 'package:devquiz_flutter/core/app_gradients.dart';
 import 'package:devquiz_flutter/core/app_images.dart';
+import 'package:devquiz_flutter/home/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then(
+      (_) => Navigator.pushReplacement(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      ),
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
